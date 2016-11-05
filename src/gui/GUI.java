3a1ce7ade8e3a1dc369cdef42c0ac939c67e616a
Addison Snyder <ajsnyde@bgsu.edu>
2016-11-05 00:54:10 -0400

    Trying a new GUI. Previous GUIs have been mostly about ease of manual testing. This one is for the final 'product'. Starting from scratch for the most part, using more organized parts of my past code.

---
+++ src/gui/GUI.java
@@ -0,0 +1,98 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JSplitPane;
+import javax.swing.UIManager;
+
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.border.BevelBorder;
+import java.awt.Dimension;
+
+public class GUI {
+
+	private JFrame frmYetAnotherYoutube;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frmYetAnotherYoutube.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public GUI() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		initialize();
+	}
+
+	private void initialize() {
+		frmYetAnotherYoutube = new JFrame();
+		frmYetAnotherYoutube.setTitle("Yet Another Youtube Downloader");
+		frmYetAnotherYoutube.setBounds(100, 100, 1600, 900);
+		frmYetAnotherYoutube.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JMenuBar menuBar = new JMenuBar();
+		frmYetAnotherYoutube.setJMenuBar(menuBar);
+
+		JPanel buttonBar = new JPanel();
+		frmYetAnotherYoutube.getContentPane().add(buttonBar, BorderLayout.NORTH);
+
+		JPanel fullPanel = new JPanel();
+		fullPanel.setBorder(null);
+
+		frmYetAnotherYoutube.getContentPane().add(fullPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_fullPanel = new GridBagLayout();
+		gbl_fullPanel.columnWidths = new int[] { 5, 0, 5, 0 };
+		gbl_fullPanel.rowHeights = new int[] { 0, 5, 5, 0 };
+		gbl_fullPanel.columnWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_fullPanel.rowWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		fullPanel.setLayout(gbl_fullPanel);
+
+		JPanel splitPaneContainer = new JPanel();
+		splitPaneContainer.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		GridBagConstraints gbc_splitPaneContainer = new GridBagConstraints();
+		gbc_splitPaneContainer.insets = new Insets(0, 0, 5, 5);
+		gbc_splitPaneContainer.fill = GridBagConstraints.BOTH;
+		gbc_splitPaneContainer.gridx = 1;
+		gbc_splitPaneContainer.gridy = 1;
+		fullPanel.add(splitPaneContainer, gbc_splitPaneContainer);
+		JPanel topPanel = new JPanel();
+		topPanel.setMinimumSize(new Dimension(0, 0));
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setMinimumSize(new Dimension(0, 0));
+		splitPaneContainer.setLayout(new BorderLayout(0, 0));
+
+		ResizableSplitPane splitPane = new ResizableSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, bottomPanel,
+				fullPanel);
+		bottomPanel.setLayout(new BorderLayout(0, 0));
+
+		JScrollPane bottomScrollPane = new JScrollPane();
+		bottomScrollPane.setMinimumSize(new Dimension(0, 0));
+		bottomPanel.add(bottomScrollPane, BorderLayout.NORTH);
+		topPanel.setLayout(new BorderLayout(0, 0));
+
+		JScrollPane topScrollPane = new JScrollPane();
+		topScrollPane.setMinimumSize(new Dimension(0, 0));
+		topPanel.add(topScrollPane, BorderLayout.CENTER);
+		splitPaneContainer.add(splitPane);
+	}
+
+}
---
+++ src/gui/ResizableSplitPane.java
@@ -0,0 +1,93 @@
+//http://stackoverflow.com/questions/36067690/how-do-you-get-jsplitpane-to-keep-the-same-proportional-location-if-the-user-has/36083987#36083987
+//http://stackoverflow.com/users/2670571/john
+package gui;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.JSplitPane;
+
+public class ResizableSplitPane extends JSplitPane {
+
+	private boolean painted;
+
+	private double defaultDividerLocation;
+
+	private double dividerProportionalLocation;
+
+	private int currentDividerLocation;
+
+	private Component first;
+
+	private Component second;
+
+	private boolean dividerPositionCaptured = false;
+
+	public ResizableSplitPane(int splitType, Component first, Component second, Component parent) {
+		this(splitType, first, second, parent, 0.5);
+	}
+
+	public ResizableSplitPane(int splitType, Component first, Component second, Component parent,
+			double defaultDividerLocation) {
+		super(splitType, first, second);
+		this.defaultDividerLocation = defaultDividerLocation;
+		this.dividerProportionalLocation = defaultDividerLocation;
+		this.setResizeWeight(defaultDividerLocation);
+		this.first = first;
+		this.second = second;
+		parent.addComponentListener(new DividerLocator());
+		second.addComponentListener(new DividerMovedByUserComponentAdapter());
+	}
+
+	public double getDefaultDividerLocation() {
+		return defaultDividerLocation;
+	}
+
+	public void setDefaultDividerLocation(double defaultDividerLocation) {
+		this.defaultDividerLocation = defaultDividerLocation;
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		if (painted == false) {
+			painted = true;
+			this.setDividerLocation(dividerProportionalLocation);
+			this.currentDividerLocation = this.getDividerLocation();
+		}
+	}
+
+	private class DividerLocator extends ComponentAdapter {
+		@Override
+		public void componentResized(ComponentEvent e) {
+			setDividerLocation(dividerProportionalLocation);
+			currentDividerLocation = getDividerLocation();
+		}
+	}
+
+	private class DividerMovedByUserComponentAdapter extends ComponentAdapter {
+		@Override
+		public void componentResized(ComponentEvent e) {
+			// System.out.println("RESIZED: " + dividerPositionCaptured);
+			int newDividerLocation = getDividerLocation();
+			boolean dividerWasMovedByUser = newDividerLocation != currentDividerLocation;
+			// System.out.println(currentDividerLocation + "\t" + newDividerLocation + "\t" + dividerProportionalLocation);
+			if (dividerPositionCaptured == false || dividerWasMovedByUser == true) {
+				dividerPositionCaptured = true;
+				painted = false;
+				if (getOrientation() == JSplitPane.HORIZONTAL_SPLIT) {
+					dividerProportionalLocation = (double) first.getWidth()
+							/ (double) (first.getWidth() + second.getWidth());
+				} else {
+					dividerProportionalLocation = (double) first.getHeight()
+							/ (double) (first.getHeight() + second.getHeight());
+
+				}
+				// System.out.println(dividerProportionalLocation);
+			}
+		}
+	}
+
+}
