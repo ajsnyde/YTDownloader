12eeee65cfccf46aab4ae36c618683ed8b06ef36
Addison Snyder <ajsnyde@bgsu.edu>
2016-12-14 19:51:41 -0500

    Got Derby over EclipseLink over JPA running well. Also added logging to persistence.xml (though it won't be integrated with FileLogger). Also added a couple gitignore items.

--- src/db/DerbyDB.java
+++ src/db/DerbyDB.java
@@ -6,62 +6,45 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.util.List;
+import java.util.logging.Level;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
+import logger.FileLogger;
 import youtubeObjects.Album;
 import youtubeObjects.Metadata;
-import youtubeObjects.Song;
 
 public class DerbyDB {
-
-  public File dbLocation = new File("db/data.db");
-  public String connectionString = "jdbc:derby:" + dbLocation.getAbsolutePath();
+  EntityManagerFactory factory;
   EntityManager em;
   Connection c;
+  Query q;
 
   public static void main(String[] args) {
     new DerbyDB();
   }
 
   DerbyDB() {
-    testEntity();
-  }
-
-  public void testEntity() {
-    EntityManagerFactory factory = Persistence.createEntityManagerFactory("derby-eclipselink");
-    em = factory.createEntityManager();
-
-    // Read the existing entries and write to console
-    Query q = em.createQuery("SELECT u FROM Metadata u");
-    List<Metadata> userList = q.getResultList();
-    System.out.println("Size: " + userList.size());
+    try {
+      EntityManagerFactory factory = Persistence.createEntityManagerFactory("derby-eclipselink");
+      em = factory.createEntityManager();
+    } catch (Exception e) {
+      FileLogger.logger().log(Level.FINER, e.getMessage());
+    }
 
     try {
       Files.find(Paths.get("Downloads"), 999, (p, bfa) -> bfa.isRegularFile() && p.toFile().getAbsolutePath().endsWith("json")).forEach(path -> addMeta(path.toFile()));
     } catch (IOException e) {
-      e.printStackTrace();
+      FileLogger.logger().log(Level.FINER, e.getMessage());
     }
-
-    q = em.createQuery("SELECT u FROM Metadata u");
-    userList = q.getResultList();
-    System.out.println("Size: " + userList.size());
-
-    addAlbum();
-
-    q = em.createQuery("SELECT u FROM Album u");
-    List<Album> albums = q.getResultList();
-    System.out.println("first album: " + albums.get(0).albumName);
-    System.out.println("Songs of first album: " + albums.get(0).songs.size());
-
-    em.close();
-    factory.close();
   }
 
   public void addMeta(File file) {
     try {
+      FileLogger.logger().log(Level.FINER, "Adding meta: " + file);
       em.getTransaction().begin();
       Metadata user = new Metadata(file);
       em.persist(user);
@@ -71,27 +54,21 @@ public class DerbyDB {
     }
   }
 
-  public void addAlbum() {
-    try {
-      em.getTransaction().begin();
-      Album album = new Album();
-      album.albumName = "TEST";
-      Song song = new Song();
-      song.album = album;
-      song.title = "TESTSONG";
-      album.songs.add(song);
-      song = new Song();
-      song.album = album;
-      song.title = "TESTSONG2";
-      album.songs.add(song);
-      em.persist(album);
-      em.getTransaction().commit();
-    } catch (Exception e) {
+  public void persist(Object obj) {
+    em.getTransaction().begin();
+    em.persist(obj);
+    em.getTransaction().commit();
+  }
 
-    }
-    Query q = em.createQuery("SELECT u FROM Album u");
+  public void numAlbums() {
+    q = em.createQuery("SELECT u FROM Album u");
     List<Album> albums = q.getResultList();
     System.out.println("first album: " + albums.get(0).albumName);
     System.out.println("Songs of first album: " + albums.get(0).songs.size());
+  }
+
+  public void close() {
+    em.close();
+    factory.close();
   }
 }
